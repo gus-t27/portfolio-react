47d5a472a005886d6b4be9a3c3db86ddc952264f
Gus Threlfall <gus.threlfall@gmail.com>
2021-01-10 15:40:16 +0000

    Navbar wip

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 export default class Navigation extends Component {
   constructor(props) {
@@ -8,10 +10,14 @@ export default class Navigation extends Component {
 
   render() {
     return (
-      <div>
-        <div className="nav-items">
-          
-        </div>
+      <div className="container">
+        <Navbar className="navbar" bg="transparent" sticky="top" variant="light">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#features">Projects</Nav.Link>
+            <Nav.Link href="#pricing">Contact</Nav.Link>
+          </Nav>
+        </Navbar>
       </div>
     )
   }
